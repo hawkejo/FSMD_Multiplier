0e92e172e570a3443e89a53c56e7a039d5f05821
Joseph H <24938582+hawkejo@users.noreply.github.com>
2025-04-02 09:55:16 -0400

    Finished multiplier design

--- FSMD_Multiplier.srcs/sources_1/new/fsm_multiply.sv
+++ FSMD_Multiplier.srcs/sources_1/new/fsm_multiply.sv
@@ -20,7 +20,161 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module fsm_multiply(
-
-    );
+module fsm_multiply(product, isDone, multiplier, multiplicand, start, clk, rst);
+    parameter WORD_SIZE = 32;
+    output reg [(2*WORD_SIZE)-1:0] product;
+    output reg isDone;
+    
+    input [WORD_SIZE-1:0] multiplier, multiplicand;
+    input start, clk, rst;      // rst is low asserted
+    
+    // State definitions
+    localparam DONE = 2'b00;
+    localparam INIT = 2'b01;
+    localparam RS   = 2'b10;
+    localparam SA   = 2'b11;
+    
+    reg [1:0] currentState, nextState;
+    reg initLoad, srSig, saSig, counterDone;
+    
+    reg [WORD_SIZE-1:0] counter;    // SystemVerilog has some new tricks, let's see how this goes...
+    wire [WORD_SIZE:0] internalSum;
+    reg [WORD_SIZE-1:0] M;
+    
+    // Handle the multiplicand
+    always_ff @ (posedge clk, negedge rst) begin
+        if(~rst) begin
+            M <= '0;
+        end
+        else begin
+            if(initLoad) begin
+                M <= multiplicand;
+            end
+        end
+    end
+    
+    // Build the adder
+    assign internalSum = product[(2*WORD_SIZE)-1:WORD_SIZE] + M;
+    
+    // Handle the product
+    always_ff @ (posedge clk, negedge rst) begin
+        if(~rst) begin
+            product <= {WORD_SIZE{1'b0}};
+        end
+        else begin
+            if(initLoad) begin
+                product <= {{WORD_SIZE{1'b0}}, multiplier};
+            end
+            else if(srSig) begin
+                product <= {1'b0, product[(2*WORD_SIZE)-1:1]};
+            end
+            else if(saSig) begin
+                product <= {internalSum, product[WORD_SIZE-1:1]};
+            end
+            else begin
+                product <= product;
+            end
+        end
+    end
+    
+    // Counter Logic
+    assign counterDone = (counter == 0)?1'b1:1'b0;
+    
+    always_ff @ (posedge clk, negedge rst) begin
+        if(~rst)
+            counter <= 0;
+        else begin
+            if(initLoad)
+                counter <= WORD_SIZE;
+            else if(counter > 0)
+                counter <= counter - 1'b1;
+        end
+    end
+    
+    // Control logic
+    always_comb begin
+        case(nextState)
+            DONE: begin
+                initLoad    = 1'b0;
+                srSig       = 1'b0;
+                saSig       = 1'b0;
+            end
+            INIT: begin
+                initLoad    = 1'b1;
+                srSig       = 1'b0;
+                saSig       = 1'b0;
+            end
+            RS: begin
+                initLoad    = 1'b0;
+                srSig       = 1'b1;
+                saSig       = 1'b0;
+            end
+            SA: begin
+                initLoad    = 1'b0;
+                srSig       = 1'b0;
+                saSig       = 1'b1;
+            end
+            default: begin
+                initLoad    = 1'b0;
+                srSig       = 1'b0;
+                saSig       = 1'b0;
+            end
+        endcase
+    end
+    
+    // Next state logic
+    always_comb begin
+        case(currentState)
+            DONE: begin
+                if(start)
+                    nextState = INIT;
+                else
+                    nextState = DONE;
+                isDone = 1'b1;
+            end
+            INIT: begin
+                if(~start & (product[0] == 1'b0))
+                    nextState = RS;
+                else if(~start & (product[0] == 1'b1))
+                    nextState = SA;
+                else
+                    nextState = INIT;
+                isDone = 1'b0;
+            end
+            RS: begin
+                if(counterDone == 1'b1)
+                    nextState = DONE;
+                else if((product[0] == 1'b0))
+                    nextState = RS;
+                else if((product[0] == 1'b1))
+                    nextState = SA;
+                else
+                    nextState = DONE;
+                isDone = 1'b0;
+            end
+            SA: begin
+                if(counterDone == 1'b1)
+                    nextState = DONE;
+                else if((product[0] == 1'b0))
+                    nextState = RS;
+                else if((product[0] == 1'b1))
+                    nextState = SA;
+                else
+                    nextState = DONE;
+                isDone = 1'b0;
+            end
+            default: begin
+                nextState = DONE;
+                isDone = 1'b1;
+            end
+        endcase
+    end
+    
+    // Handle FSM flippy boiz
+    always_ff @ (posedge clk, negedge rst) begin
+        if(~rst)
+            currentState <= DONE;
+        else
+            currentState <= nextState;
+    end
 endmodule
--- FSMD_Multiplier.srcs/sources_1/new/main.sv
+++ FSMD_Multiplier.srcs/sources_1/new/main.sv
@@ -20,7 +20,88 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module main(
-
-    );
+module main(LED, DP, AN, SEG, SW, start, setDivisor, setDividend, upper2bytes, clk, rst,
+            LED16_B, LED16_G, LED16_R, LED17_B, LED17_G, LED17_R);
+    output DP;
+    output [6:0] SEG;
+    output [7:0] AN;
+    output LED16_B, LED16_G, LED16_R, LED17_B, LED17_G, LED17_R;
+    output [15:0] LED;
+    input [15:0] SW;
+    input start, setDivisor, setDividend, upper2bytes, clk, rst;
+    
+    reg [31:0] divisor, dividend;
+    
+    wire invRst;
+    wire debStart, debDivisor, debDividend, debUpperBytes;
+    wire [31:0] qOut;//, remOut;
+    
+    assign invRst = !rst;
+    
+    // Debounce the start button
+    debouncer deb0 (.debBtn(debStart), .clk100MHz(clk), .btn(start), .rst(rst));
+    
+    // Debounce the divisor and dividend control buttons.
+    debouncer deb1 (.debBtn(debDivisor), .clk100MHz(clk), .btn(setDivisor), .rst(rst));
+    debouncer deb2 (.debBtn(debDividend), .clk100MHz(clk), .btn(setDividend), .rst(rst));
+    debouncer deb3 (.debBtn(debUpperBytes), .clk100MHz(clk), .btn(upper2bytes), .rst(rst));
+    
+    // I/O logic for handling setting the divisor and dividend registers
+    always @(posedge clk, posedge rst) begin
+        if (rst) begin
+            divisor <= 0;
+            dividend <= 0;
+        end
+        else if (debDivisor) begin
+            if (debUpperBytes)
+                divisor[31:16] <= SW;
+            else
+                divisor[15:0] <= SW;
+        end
+        else if (debDividend) begin
+            if (debUpperBytes)
+                dividend[31:16] <= SW;
+            else
+                dividend[15:0] <= SW;
+        end
+        else begin
+            divisor <= divisor;
+            dividend <= dividend;
+        end
+    end
+    
+    // Status indicator LED logic
+    assign LED17_B = (dividend == 0)?1'b1:1'b0;
+    assign LED17_R = ((dividend[15:0] == 0) && (dividend[31:16] != 0))?1'b1:1'b0;
+    assign LED17_G = ((dividend[15:0] != 0) && (dividend[31:16] == 0))?1'b1:1'b0;
+    
+    assign LED16_B = (divisor == 0)?1'b1:1'b0;
+    assign LED16_R = ((divisor[15:0] == 0) && (divisor[31:16] != 0))?1'b1:1'b0;
+    assign LED16_G = ((divisor[15:0] != 0) && (divisor[31:16] == 0))?1'b1:1'b0;
+    
+    // The divider module
+    fsm_multiply div0(.product({LED,qOut}),  .multiplier(divisor), .isDone(),
+                 .multiplicand(dividend), .start(debStart), .clk(clk), .rst(invRst));
+    
+    // Display the remainder on the LEDs.  The upper 16 bits are truncated.
+    //assign LED = remOut[15:0];
+    
+    // 7-segment display modules for displaying the quotient
+    wire dispClk;
+    wire [7:0] a, b, c, d, e, f, g, h;
+    clockDivider redClk0(dispClk, clk, rst);
+    
+    sevenSegmentControl disp0(.dp(DP), .seg(SEG), .an(AN),
+                .clk(dispClk), .dispUsed(8'h00), .data0(a), .data1(b), .data2(c),
+                .data3(d), .data4(e), .data5(f), .data6(g), .data7(h));
+    
+    sevenSegmentHex disp1(.segData(a), .number(qOut[3:0]));
+    sevenSegmentHex disp2(.segData(b), .number(qOut[7:4]));
+    sevenSegmentHex disp3(.segData(c), .number(qOut[11:8]));
+    sevenSegmentHex disp4(.segData(d), .number(qOut[15:12]));
+    sevenSegmentHex disp5(.segData(e), .number(qOut[19:16]));
+    sevenSegmentHex disp6(.segData(f), .number(qOut[23:20]));
+    sevenSegmentHex disp7(.segData(g), .number(qOut[27:24]));
+    sevenSegmentHex disp8(.segData(h), .number(qOut[31:28]));
+    
 endmodule
